fccef46c891a699d9a155e13327fc1b3099ed2bd
Ethan Fisher <ethan.fisher@cenovus.com>
2024-06-24 08:56:18 -0600

    Green test

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="min-h-screen bg-blue-300">
+    <div className="min-h-screen bg-green-300">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
