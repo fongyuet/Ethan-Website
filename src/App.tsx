e4509092a332853a20fe793e23c0c32d7262d62b
Ethan Fisher <ethan.fisher@cenovus.com>
2024-06-24 09:18:30 -0600

    red

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="min-h-screen bg-green-300">
+    <div className="min-h-screen bg-red-300">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
